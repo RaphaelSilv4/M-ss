8c7af7b088d6d1e5b42ce0c72819054a434e11b1
Jose-Luan <123100812+Jose-Luan@users.noreply.github.com>
2024-06-18 16:41:45 -0300

    Update app_config.py

--- server/app_config.py
+++ server/app_config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:hot4@127.0.0.1:5432/m&s'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:password@127.0.0.1:5432/m&s'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY ='SECRET'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
